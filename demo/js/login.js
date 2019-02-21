fff52a5b57e5461e4ea59bb3b860783aafae7960
biaoalice <1229083434@qq.com>
2019-02-21 23:32:02 +0800

    login.js

--- demo/js/login.js
+++ demo/js/login.js
@@ -1,43 +1,27 @@
-let li = document.getElementsByTagName('li');
-let inp = document.getElementsByTagName('input');
-let btn = document.querySelector('#submit');
-for(let i=0;i<li.length;i++){
-    li[i].addEventListener('click',function(){
-        for(let j=0;j<li.length;j++){
-            li[j].classList.remove('active');
+$(function(){
+    $('#login').on('click',function(){
+        let idReg = /^\d{12}$/,
+            psdReg = /^\d{6,10}$/, 
+            id = $('#userId').val(),
+            psd = $('#userPsd').val();
+        if(idReg.test(id) && psdReg.test(psd)){
+            $('.alert').css("display","block");
+            $('.alert').removeClass('alert-danger'),
+            $('.alert').addClass('alert-success');
+            $('.alert').html('登录成功');
+            setTimeout(function(){
+                $('.alert').css("display","none");
+            },2000)
+        }else{
+            $('.alert').css("display","block");
+            $('.alert').removeClass('alert-success'),
+            $('.alert').addClass('alert-danger');
+            $('.alert').html('请输入正确的学号/密码');
+            setTimeout(function(){
+                $('.alert').css("display","none");
+            },2000)
+            $("#userId").val('');
+            $("#userPsd").val('');
         }
-        this.classList.add('active');
     })
-}
-
-btn.addEventListener('click',function(){
-    let user= inp[0].value;
-    let psd = inp[1].value;
-    if(user== '' && psd == ''){
-        alert('不能为空！！')
-        return;
-    }
-    // 
-   if(li[0].className == 'active'){
-        toAjax(1,user,psd);
-   }else{
-        toAjax(2,user,psd);
-        console.log(2);
-   }
 })
-
-function toAjax(x,y,z){
-    $.ajax({
-        type:'GET',
-        url:'http://localhost:8080/math/login.php',
-        data:{id:x,userNum:y,psd:z},
-        success(data){
-            if(data==1){
-                alert('登陆成功！！')
-            }else{
-                alert('登陆失败！！')
-            }
-            console.log(data);
-        }
-    })
-}
